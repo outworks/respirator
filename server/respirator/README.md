eea2a49c85d186ed95f2a5ca8eb50cbf40102146
ilikeido <ilikeido@163.com>
2015-06-11 08:28:07 +0800

    初始化

---
+++ server/respirator/README.md
@@ -0,0 +1,3 @@
+# respirator
+
+a [Sails](http://sailsjs.org) application
---
+++ server/respirator/api/models/Member.js
@@ -0,0 +1,31 @@
+/**
+* Member.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+	mid:{
+	 type:'integer',
+         autoIncrement:true
+        },username:{
+	  type:'string',
+	  unique:true
+	},nickname:{
+	  type:'string',
+	  size:24
+	},password:{
+	  type:'string'
+	},email:{
+	   type:'email',
+	   unique:true
+	},phone:{
+	   type:'string',
+	   size:11
+	}
+  }
+};
+
